da64b4b18fa8ac51a39a6410ef4a8f3c58d02515
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-09-16 21:04:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -36,11 +36,6 @@
         video.src = URL.createObjectURL(target.files[0]);
     });
     const video = $('<video>').appendTo(body.append('<br>')).get(0);
-    const limit100 = rpgen3.addInputBool(body, {
-        label: '100制限',
-        save: true,
-        value: true
-    });
     const is12 = rpgen3.addInputBool(body, {
         label: '高さ12',
         save: true
@@ -51,6 +46,17 @@
         min: 1,
         max: 60,
         value: 8
+    });
+    const inputType = rpgen3.addSelect(body, {
+        label: '色比較アルゴリズム',
+        save: true,
+        list: {
+            'RGB表色系でのユークリッド距離による色差の計算' : 3,
+            'XYZ表色系でのユークリッド距離による色差の計算' : 2,
+            'L*a*b*表色系でのユークリッド距離による色差の計算' : 1,
+            'CIEDE2000による色差の計算' : 0
+        },
+        value: 0
     });
     addBtn(body.append('<br>'), '映写開始', () => main()).css({
         color: 'white',
@@ -63,7 +69,8 @@
               height = is12 ? 12 : 11,
               cv = $('<canvas>').prop({width, height}),
               ctx = cv.get(0).getContext('2d'),
-              mass = [...new Array(300)].map(v => [...new Array(300)]);
+              yuka = [...new Array(300)].map(v => [...new Array(300)]),
+              mono = [...new Array(300)].map(v => [...new Array(300)]);
         for(let y = 0; y < 20; y++) {
             for(let x = 0; x < 25; x++) {
                 const now = x + y * 20;
@@ -73,24 +80,27 @@
                       {data} = imgData;
                 for(let i = 0; i < data.length; i += 4) {
                     const _x = (i >> 2) % width,
-                          _y = (i >> 2) / 12 | 0;
-                    mass[y + _y][x + _x] = getColor(data.slice(i, i + 4));
+                          _y = (i >> 2) / 12 | 0,
+                          output = getSprite(...data.slice(i, i + 3), inputType());
+                    yuka[y + _y][x + _x] = output[3];
+                    mono[y + _y][x + _x] = output[4];
                 }
                 await dialog(`${now}/500`);
             }
         }
-        floor = mass.map(v => v.join(' ')).join('\n');
+        mono[7][3] = (mono[7][3] || 45) + 'C';
+        g_floor = yuka.map(v => v.join(' ')).join('\n');
+        g_map = mono.map(v => v.join(' ')).join('\n');
         await dialog(`映写完了`);
     };
-    let floor;
-    const getColor = ([r, g, b, a]) => {
-        return 45;
-    };
+    let g_floor, g_map;
+    const {getSprite} = await import('https://rpgen3.github.io/projector/mjs/getSprite.mjs');
     const inputDelay = rpgen3.addInputNum(body, {
         label: '遅延修正[ms]',
         save: true,
         min: 0,
-        max: 100
+        max: 100,
+        value: 0
     });
     addBtn(body.append('<br>'), '出力', () => output()).css({
         color: 'white',
@@ -114,11 +124,10 @@
             }
             evts.push(`#WAIT\nt:${wait},`);
         }
-        const map = '\n'.repeat(7) + ' '.repeat(3) + '45C';
         const mapData = [
             await(await fetch('data.txt')).text(),
-            `#FLOOR\n${floor}#END`,
-            `#MAP\n${map}#END`,
+            `#FLOOR\n${g_floor}#END`,
+            `#MAP\n${g_map}#END`,
             new rpgen.FullEvent().make(['#CH_PH\np:0,x:0,y:0,'], 3, 7),
             new rpgen.FullEvent(10).make(evts)
         ].join('\n\n');
@@ -127,8 +136,8 @@
             copy: true
         });
     };
-    const rpgen = await Promise.all([
-        './export/rpgen.mjs',
-        './export/fullEvent.mjs'
-    ].map(v=>import(v))).then(v => Object.assign({},...v));
+    const rpgen = await importAll([
+        'rpgen',
+        'fullEvent'
+    ].map(v=>`https://rpgen3.github.io/midi/export/${v}.mjs`));
 })();
