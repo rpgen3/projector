899bc847e242a24346c1bd9ca4fc2cc1a69a3078
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-09-17 20:15:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -134,7 +134,13 @@
         evts.push(`#MV_CA\ntx:7,ty:5,t:0,s:1,`);
         evts.push(`#MV_PA\ntx:9999,ty:9999,t:0,n:1,s:1,`);
         evts.push(`#CH_YB\nv:${rpgen3.getParam(inputURL()).v},`);
-        evts.push(`#WAIT\nt:4000,`);
+        evts.push(`#WAIT\nt:1000,`);
+        evts.push(`#PS_YB`);
+        evts.push(`#WAIT\nt:1000,`);
+        evts.push(`#PS_YB`);
+        evts.push(`#WAIT\nt:1000,`);
+        evts.push(`#PS_YB`);
+        evts.push(`#WAIT\nt:1000,`);
         evts.push(`#PS_YB`);
         evts.push(`#WAIT\nt:500,`);
         evts.push(`#SK_YB\ns:0,`);
