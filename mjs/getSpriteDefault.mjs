0ce9ef2448013c125f4878dc3ad4d615b0acd6c5
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-09-29 00:34:19 +0900

    Update getSpriteDefault.mjs

--- mjs/getSpriteDefault.mjs
+++ mjs/getSpriteDefault.mjs
@@ -1,17 +1,4 @@
 import {diffColor} from 'https://rpgen3.github.io/projector/mjs/diffColor.mjs';
-const getTrendCode = (r, g, b) => {
-    let s = '';
-    if(r === g) s += 'a';
-    if(r === b) s += 'b';
-    if(g === b) s += 'c';
-    if(r < b) s += 'd';
-    if(r > b) s += 'e';
-    if(r < g) s += 'f';
-    if(r > g) s += 'g';
-    if(g < b) s += 'h';
-    if(g > b) s += 'i';
-    return s;
-};
 const dic = await Promise.all([
     'average',
     'median',
@@ -25,23 +12,13 @@ const dic = await Promise.all([
     }
     return map;
 }));
-const list = dic.slice().map(v => new Map);
-const add = (map, r, g, b, yuka) => {
-    const code = getTrendCode(r, g, b);
-    if(!map.has(code)) map.set(code, []);
-    map.get(code).push([r, g, b, yuka]);
-};
-for(const [i, map] of list.entries()) for(const [k, v] of dic[i]) add(map, ...v, k);
 export const getSpriteDefault = (r, g, b, type = 0, ex = 0) => {
-    const m = list[ex],
-          code = getTrendCode(r, g, b);
-    if(!m.has(code)) throw 'missing dic';
     let min = 1, output = null;
-    for(const v of m.get(code)) {
+    for(const [k, v] of dic[ex]) {
         const dif = diffColor([r, g, b], v, type);
         if(min > dif) {
             min = dif;
-            output = v;
+            output = k;
         }
     }
     return output;
